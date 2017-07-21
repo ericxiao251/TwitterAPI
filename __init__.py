d995ce388e6dc2a272b64ad5ff2a522719f3982f
eric xiao <xiaoe@mcmaster.ca>
2017-07-21 00:19:31 -0400

    First Twitter REST API Class: search/tweets.

--- __init__.py
+++ __init__.py
@@ -38,6 +38,18 @@ class TwitterGetRequest(TwitterRestApiBaseClass):
 		return self.twitter.__get__(url)
 
 
+class Search(TwitterGetRequest):
+	def tweet(self, q, **kwargs):
+		if not q:
+			raise ValueError('No parameter q passed in.')
+
+		request = str(self.twitter.base_url + '/search/tweets.json?q=' + q)
+		for key, value in kwargs.items():
+			request = str('&' + request + key + '=' + value)
+
+		return self.__get__(request)
+
+
 if __name__ == '__main__':
 	CONSUMER_KEY = 'i6osYlAMvLyiSFYV56eHEcA50'
 	CONSUMER_SECRET = 'A67oLT5d2NzTZc6xTbYGoJI8GDecRHRpJ8hFJenMM8lfbqLOBA'
@@ -45,3 +57,6 @@ if __name__ == '__main__':
 	OAUTH_TOKEN_SECRET = 'p7PV5i3Gdz5mEXsbBSJG1PzJ1rU6woKe8359r1cCWaecD'
 
 	twitter_object = Twitter(CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+	tweet = Search(twitter_object).tweet('%23freebandnames')
+
+	print(tweet)
