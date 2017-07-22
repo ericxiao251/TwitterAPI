35fa62e23b6d50b66f875203f8f32e5ced17535a
eric xiao <xiao.eric251@gmail.com>
2017-07-21 21:26:57 -0400

    storing keys in secrets.json

--- __init__.py
+++ __init__.py
@@ -1,11 +1,16 @@
+import json
+
 from twitter import Twitter
 from twitter.search import Search
 
 if __name__ == '__main__':
-    CONSUMER_KEY = 'i6osYlAMvLyiSFYV56eHEcA50'
-    CONSUMER_SECRET = 'A67oLT5d2NzTZc6xTbYGoJI8GDecRHRpJ8hFJenMM8lfbqLOBA'
-    OAUTH_TOKEN = '843826210820055041-w0uUuj1xNGp9kdu4pwsLKHHc4pwZd5z'
-    OAUTH_TOKEN_SECRET = 'p7PV5i3Gdz5mEXsbBSJG1PzJ1rU6woKe8359r1cCWaecD'
+    with open('secrets.json') as f:
+        config = json.load(f)
+
+    CONSUMER_KEY = config['consumer_key']
+    CONSUMER_SECRET = config['consumer_secret']
+    OAUTH_TOKEN = config['oauth_token']
+    OAUTH_TOKEN_SECRET = config['oauth_token_secret']
 
     twitter_object = Twitter(CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
     tweet = Search(twitter_object).tweet(q='%23freebandnames')
