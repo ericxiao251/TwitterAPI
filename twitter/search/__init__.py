415499352de6411c152c9f3f197c87862d4e8eff
eric xiao <xiao.eric251@gmail.com>
2017-07-23 14:50:01 -0400

    Adding a description.

--- twitter/search/__init__.py
+++ twitter/search/__init__.py
@@ -5,6 +5,9 @@ class Search(TwitterRestApiBaseClass):
     resource = 'search/'
 
     def tweets(self, **kwargs):
+        """
+        Returns a collection of relevant Tweets matching a specified query.
+        """
         resource = self.resource + 'tweets'
         required = ['q']
         metadata, response = self.__get__(resource, required, kwargs)  # use metadata for something else.
